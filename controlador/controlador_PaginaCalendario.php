2fe269411b75c70c2fd77f078bf53ee690d6855b
Raul Nubelfon <raul.aquilue.nubelfon@gmail.com>
2021-11-11 19:42:08 +0100

    mock get recordatorios

--- controlador/controlador_PaginaCalendario.php
+++ controlador/controlador_PaginaCalendario.php
@@ -6,8 +6,33 @@ $connexio=connectDB();
 
 require_once __DIR__.'/../vista/vista_cabecera.php';
 
-
-
 require_once __DIR__.'/../vista/vista_PaginaCalendario.php';
+
+
+function getRecordatorios($user_id){
+    //Devolverá un listado de recordatorios
+}
+
+function getRecordatoriosMock($user_id){
+    if($user_id==1){
+        $recordatorios= array(
+            '2021-02-26 10:30' => [['titulo1', 'desc1'], ['titulo2', 'desc2']] ,
+            '2021-05-13 08:55' => [['titulo', 'desc']],
+            '2021-07-01 23:55' => [['titulo3', 'desc3'], ['titulo4', 'desc4']] ,
+            '2021-02-26 11:30' => [['titulo5', 'desc5']],
+        );
+        ksort($recordatorios);
+    }else{
+        $recordatorios=array();
+    }
+    return $recordatorios;
+}
+$recordatorios=getRecordatoriosMock(1);
+foreach(array_keys($recordatorios) as $fecha_key){
+    var_dump($fecha_key);
+    echo " - ";
+    var_dump($recordatorios[$fecha_key]);
+    echo "<br>";
+}
 
 ?>
