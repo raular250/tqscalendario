c26660135dbe056a284823343dfdc03cb81222da
Raul Nubelfon <raul.aquilue.nubelfon@gmail.com>
2021-11-14 19:26:04 +0100

    vista calendario y primer intento funciones calendario

--- controlador/controlador_PaginaCalendario.php
+++ controlador/controlador_PaginaCalendario.php
@@ -22,30 +22,123 @@ function getRecordatoriosMock($user_id){
 }
 function minusAnt($nextRec,$ant){
     //Restar a nextRec la anterioridad de $ant;
+    if($nextRec!=false){
+
+    }
     return $nextRec;
 }
 function nextRec($ini,$fin,$freq,$ant,$date){
-    switch($freq){//1D,2D,3D..,1M,2M,3M...,1A,2A,3A...,once,daily,L-V",annually
-        case "once":
-            if($ini<$date){
-                $nextRec=false;
-            }else{
-                minusAnt($nextRec,$ant);
-                $nextRec=date('Y-m-d H:i', strtotime($ini. ' - 5 days'));
+    if(($ini<$fin) and ($date<$fin) ){
+        if(is_numeric($freq[0])){    //1D,2D,3D..,1M,2M,3M...,1A,2A,3A...,
+            switch($freq[strlen($freq)-1]){
+                case 'D':
+                    if($ini>$date){
+                        $nextRec=$ini;
+                    }else{
+                        $nextRec=$ini;
+                        do{
+                            $nextRec=date('Y-m-d H:i', strtotime($nextRec. '+ '.substr($freq,0,-1).' days'));
+                        }while($date<$ini);
+                    }
+                    $nextRec=minusAnt($nextRec,$ant);
+                    if($nextRec>$fin){
+                        $nextRec=false;
+                    }
+                    break;
+                case 'M':
+                    if($ini>$date){
+                        $nextRec=$ini;
+                    }else{
+                        $nextRec=$ini;
+                        do{
+                            $nextRec=date('Y-m-d H:i', strtotime($nextRec. '+ '.substr($freq,0,-1).' months'));
+                        }while($date<$ini);
+                    }
+                    $nextRec=minusAnt($nextRec,$ant);
+                    if($nextRec>$fin){
+                        $nextRec=false;
+                    }
+                    break;
+                case 'A':
+                    if($ini>$date){
+                        $nextRec=$ini;
+                    }else{
+                        $nextRec=$ini;
+                        do{
+                            $nextRec=date('Y-m-d H:i', strtotime($nextRec. '+ '.substr($freq,0,-1).' years'));
+                        }while($date<$ini);
+                    }
+                    $nextRec=minusAnt($nextRec,$ant);
+                    if($nextRec>$fin){
+                        $nextRec=false;
+                    }
+                    break;
             }
-            break;
-        case "daily":
-            break;
-        case "L-V":
-            break;
-        case "annually":
-            break;
+        }else{
+            switch($freq){  //once,daily,L-V",annually
+                case "once":
+                    if($ini>$date){
+                        $nextRec=$ini;
+                        $nextRec=minusAnt($nextRec,$ant);
+                    }else{
+                        $nextRec=false;
+                    }
+                    break;
+                case "daily":
+                    if($ini>$date){
+                        $nextRec=$ini;
+                    }else{
+                        $nextRec=$ini;
+                        do{
+                            $nextRec=date('Y-m-d H:i', strtotime($nextRec. '+ 1 days'));
+                        }while($date<$ini);
+                    }
+                    $nextRec=minusAnt($nextRec,$ant);
+                    if($nextRec>$fin){
+                        $nextRec=false;
+                    }
+                    break;
+                case "L-V":
+                    if($ini>$date){
+                        $nextRec=$ini;
+                    }else{
+                        $nextRec=$ini;
+                        do{
+                            $nextRec=date('Y-m-d H:i', strtotime($nextRec. '+ 1 days'));
+                            $weekDay=date('w',$nextRec);
+                        }while($date<$ini and $weekDay>=1 and $weekDay<=5);
+                    }
+                    $nextRec=minusAnt($nextRec,$ant);
+                    if($nextRec>$fin){
+                        $nextRec=false;
+                    }
+                    break;
+                case "annually":
+                    if($ini>$date){
+                        $nextRec=$ini;
+                    }else{
+                        $nextRec=$ini;
+                        do{
+                            $nextRec=date('Y-m-d H:i', strtotime($nextRec. '+ 1 year'));
+                            $weekDay=date('w',$nextRec);
+                        }while($date<$ini);
+                    }
+                    $nextRec=minusAnt($nextRec,$ant);
+                    if($nextRec>$fin){
+                        $nextRec=false;
+                    }
+                    break;
+            }
+        }
     }
-    $nextRec=date('Y-m-d H:i', strtotime($date. ' + 5 days'));
+    if(!isset($nextRec)){
+        $nextRec=false;
+    }
     return $nextRec;
 }
 function getRecordatorios($user_id,$connexio){
     $recordatorios=getRecordatoriosBD($connexio,$user_id);
+    $arrayRec=array();
     foreach($recordatorios as $r){
         $titulo=$r[1];
         $ini=$r[2];
@@ -55,13 +148,32 @@ function getRecordatorios($user_id,$connexio){
         $desc=$r[6];
         $date=date("Y-m-d H:i");
         $nextRec=nextRec($ini,$fin,$freq,$ant,$date);
+        if($nextRec!=false){
+            if(array_key_exists($nextRec,$arrayRec)){
+                // var_dump($arrayRec[$nextRec]);
+                // echo "</br></br>";
+                // echo "</br></br>";
+                array_push($arrayRec[$nextRec],[$titulo,$desc]);
+                // var_dump($arrayRec[$nextRec]);
+                // echo "</br></br>";
+                // echo "</br></br>";
+    
+            }else{
+                $arrayRec[$nextRec]=array(array($titulo,$desc));
+            }
+        }
     }
-    $recordatorios=array();
-    return $recordatorios;
+    // foreach(array_keys($arrayRec) as $k){
+    //     echo "[$k]->";
+    //     var_dump($arrayRec[$k]);
+    //     echo "</br></br>";
+    // }
+    if(count($arrayRec) >=1) ksort($arrayRec);
+
+    return $arrayRec;
 }
 $user_id=(isset($_SESSION['userId'])?$_SESSION['userId']:-1);
 $recordatorios=getRecordatorios($user_id,$connexio);
-
 
 require_once __DIR__.'/../vista/vista_PaginaCalendario.php';
 
--- vista/vista_PaginaCalendario.php
+++ vista/vista_PaginaCalendario.php
@@ -3,20 +3,35 @@
 
     <h2>CALENDARIO</h2>
     <br>
-<?php
-$contador=0;
-        foreach(array_keys($recordatorios) as $key){ 
-            $contador=$contador+1; ?>
-            <h3>Recordatorio número <?php echo ($contador) ?>
-            <div class="fechaCalendario"> FECHA: <?php print_r($key); ?> </div> <br> <?php
+    <table class='calendario'>
+        <?php
+            foreach(array_keys($recordatorios) as $key){
+            ?>
+            <tr>
+                <th><?php print_r($key) ?></th>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
+                <?php foreach(array_values($recordatorios[$key]) as $valor ){ ?>
+                    <tr>
+                        <th></th>
+                        <th><?php print_r($valor[0]);?>
+                        =>
+                        <?php print_r($valor[1]);?></th>
+                    </tr>
+                <?php } ?>
 
-        foreach(array_values($recordatorios[$key]) as $valor ){ ?>
-            <div class="tituloCalendario"> Título: <?php print_r($valor[0]); ?> </div> <br> 
-            <div class="descripcionCalendario"> Descripción: <?php print_r($valor[1]); ?> </div> <br> <?php
-            echo "<br>";
-        }
-        echo "<br>";echo "<br>";
-    }
-?>
 
+                <tr>
+                    <th><br></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+
+            <?php
+            }
+        ?>
+    </table>
 </body>
