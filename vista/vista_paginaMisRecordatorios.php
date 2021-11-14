69257c729bf23740a9391e484366da55ec96bbfa
Raul Nubelfon <raul.aquilue.nubelfon@gmail.com>
2021-11-14 21:11:05 +0100

    edit vista MisRecordatorios

--- vista/vista_paginaMisRecordatorios.php
+++ vista/vista_paginaMisRecordatorios.php
@@ -1,17 +1,29 @@
 <body class="center">
 
     <h2>MIS RECORDATORIOS </h2>
-
+    <table class="recordatorios">
+    <tr>
+        <th>titulo</th>
+        <th>fecha inicio</th>
+        <th>fecha final</th>
+        <th>frequencia</th>
+        <th>anterioridad</th>
+        <th>descripción</th>
+    </tr>
     <?php
 
     foreach(array_keys($misRecordatorios) as $k){
+        echo "<tr>";
         foreach(array_keys($misRecordatorios[$k]) as $v){
-            echo($misRecordatorios[$k][$v]);
+            if($v >=1 and $v<=5)
+                echo '<td>'.$misRecordatorios[$k][$v]."</td> \n";
+            if($v==6)
+                echo "<td class='descriptionCol'>".$misRecordatorios[$k][$v]."</td> \n";
             
         }
-    echo "<br>";echo "<br>";
+        echo "</tr> \n\n";
     }
     ?>
 
-
+    </table>
 </body>
